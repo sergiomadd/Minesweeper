5268eddd3c0018e3863dac2d143960ce268b0374
innib <innib@10.109.50.215>
2020-03-05 18:51:40 +0100

    Se ha añadido la vista de juego preliminar. Cambios: -Matriz de casillas de 7x10. -Ahora las casillas cambian al estar marcadas. -Se han añadido el botón de reset(sin funcionalidad) y los huecos para la puntuación y el tiempo.

--- Minesweeper/src/com/Graphics/VistaJuego.java
+++ Minesweeper/src/com/Graphics/VistaJuego.java
@@ -32,11 +32,51 @@ public class VistaJuego extends JFrame {
 	 */
 	public VistaJuego() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 400, 400);
+		setResizable(false);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel Grid = new JPanel();
+		contentPane.add(Grid, BorderLayout.CENTER);
+		Grid.setLayout(new GridLayout(7,10, 0, 0));
+		
+		JPanel Menu = new JPanel();
+		contentPane.add(Menu, BorderLayout.NORTH);
+		Menu.setLayout(new GridLayout(0, 3, 0, 0));
+		
+		JLabel lblPuntos = new JLabel("puntos");
+		lblPuntos.setHorizontalAlignment(SwingConstants.CENTER);
+		Menu.add(lblPuntos);
+		
+		JButton btnReset = new JButton("RESET");
+		btnReset.setIcon(new ImageIcon(window.class.getResource("/org/eclipse/jface/dialogs/images/help.png")));
+		Menu.add(btnReset);
+		
+		
+		JLabel lblTiempo = new JLabel("tiempo");
+		lblTiempo.setHorizontalAlignment(SwingConstants.CENTER);
+		Menu.add(lblTiempo);
+		for(int i=0; i < x ; i++){
+			for(int j=0; j < y; j++){
+				JButton btnNewButton = new JButton("");
+				Grid.add(btnNewButton);
+				Color sinmarcar = new Color(220,220,220);
+				btnNewButton.setBackground(sinmarcar);
+				btnNewButton.setBorder(new LineBorder(Color.WHITE));
+				btnNewButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						 Color marcado = new Color(169,169,169);
+						btnNewButton.setBackground(marcado);
+						btnNewButton.setBorder(new LineBorder(Color.DARK_GRAY));
+					}
+				});
+				//comentario
+			}
+		}
+
 	}
 
 }
