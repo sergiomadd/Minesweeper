84204bca99ac35520910d6e9a7a5af78e0a69cb6
sergiomadd <sergiogitarrista@gmail.com>
2020-04-01 12:14:04 +0100

    Creada vista inicial

--- Minesweeper/src/com/Graphics/VistaInicial.java
+++ Minesweeper/src/com/Graphics/VistaInicial.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class VistaInicial extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -35,8 +41,42 @@ public class VistaInicial extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(5, 0, 0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		
+		JLabel lblIntroduceTuNombre = new JLabel("Introduce tu nombre");
+		panel.add(lblIntroduceTuNombre);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1);
+		
+		textField = new JTextField();
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panel_2 = new JPanel();
+		contentPane.add(panel_2);
+		
+		JLabel lblDificultad = new JLabel("Dificultad");
+		panel_2.add(lblDificultad);
+		
+		JPanel panel_3 = new JPanel();
+		contentPane.add(panel_3);
+		
+		textField_1 = new JTextField();
+		panel_3.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JPanel panel_4 = new JPanel();
+		contentPane.add(panel_4);
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		panel_4.add(btnAceptar);
+		
+		setVisible(true);
 	}
 
 }
--- Minesweeper/src/com/Main.java
+++ Minesweeper/src/com/Main.java
@@ -1,5 +1,6 @@
 package com;
 
+import com.Graphics.VistaInicial;
 import com.Graphics.VistaJuego;
 
 public class Main {
@@ -7,5 +8,6 @@ public class Main {
 	public static void main(String[] args) {
 		Controlador.getControlador().iniciarPartida();
 		VistaJuego vistaJuego = new VistaJuego(7, 10, Buscaminas.getBuscaminas().getTab());
+		//VistaInicial vistaInicial = new VistaInicial();
 	}
 }
